e04dc876883ab35343e9f540932a2dca95424ff1
LennartPurucker <lennart.purucker@uni-siegen.de>
2022-02-01 15:05:14 +0100

    changed ranking plots and added information

--- evaluation/eval_plotter.py
+++ evaluation/eval_plotter.py
@@ -88,52 +88,99 @@ def ranking_eval(data: pd.DataFrame, save_images, prefix=""):
     # -- Save Tables
     res_table_top_10_models.to_csv(os.path.join(get_base_path(), get_output_result_tables(), "rank_top_10_models.csv"))
     res_table_rank_per_cat.to_csv(os.path.join(get_base_path(), get_output_result_tables(), "rank_per_cat.csv"))
-    exit()
 
     # --- Rank Plots
-
     # Reformat and Build data
-    top_5_model_per_dataset = {}
-    top_5_model_name_per_dataset = {}
+    top_5_category_per_dataset = {}
+    top_5_stats_per_dataset = {}
     libcat_ranking_per_dataset = {}
+    top_5_stats_per_cat_per_dataset = {}
     for dataset_name in data["Dataset"].unique().tolist():
         tmp_df = data[data["Dataset"] == dataset_name]
         tmp_df = tmp_df.sort_values(by="RMSE_RANK")
 
-        top_5_model_per_dataset[dataset_name] = tmp_df["LibraryCategory"].iloc[0:5].to_list()
-        top_5_model_name_per_dataset[dataset_name] = tmp_df["Model"].iloc[0:5].to_list()
-
+        # Get Values for Heatmap colors
+        top_5_category_per_dataset[dataset_name] = tmp_df["LibraryCategory"].iloc[0:5].to_list()
+        # Top 5 unique
         x = tmp_df["LibraryCategory"].to_list()
         libcat_ranking_per_dataset[dataset_name] = sorted(set(x), key=x.index)
 
-    data_for_rank_plot_models = pd.DataFrame.from_dict(top_5_model_per_dataset, orient="index",
-                                                       columns=["1", "2", "3", "4", "5"])
+        # Get Stats for annotation for Top 5 algorithms and top 5 categories
+        top_5_stats_per_dataset[dataset_name] = tmp_df[["Model", "RSME"]].iloc[0:5].values.tolist()
+
+        # Get index for sorted set of categories to find correct values
+        top_5_cats = set()
+        top_5_cat_index = []
+        for i, cat in enumerate(x, 0):
+            if cat not in top_5_cats:
+                top_5_cats.add(cat)
+                top_5_cat_index.append(i)
+        top_5_stats_per_cat_per_dataset[dataset_name] = [tmp_df[["Model", "RSME"]].iloc[i].values.tolist()
+                                                         for i in top_5_cat_index]
+
+    # Build heatmap colors for category rankings
     nr_cats = len(data["LibraryCategory"].unique().tolist())
     data_for_rank_plot_libcat = pd.DataFrame.from_dict(libcat_ranking_per_dataset, orient="index",
                                                        columns=[str(i) for i in range(1, nr_cats + 1)])
-    # Print Top 5 model names to look for variance
-    print("####### Model Names top 5 #######")
-    for key, item in top_5_model_name_per_dataset.items():
-        print("Dataset: {} | {}".format(key, item))
+    # Build heatmap color data for top algorithms per dataset
+    data_for_rank_plot_models = pd.DataFrame.from_dict(top_5_category_per_dataset, orient="index",
+                                                       columns=["1", "2", "3", "4", "5"])
+
+    # Add annotation
+    def replace_names_for_paper(specific_name):
+        # A hand made function to change the internal names of algorithms to the names used in the paper
+
+        # filter special cases
+        if specific_name == "H2O_AutoML":
+            return "H20AutoML"
+        if "_" in specific_name:
+            n_prefix, n_postfix = specific_name.split("_")
+
+            # Change postfix
+            if n_postfix == "SingularValueDecompositionAlgorithm":
+                n_postfix = "SVD"
+
+            # Filter prefix
+            if n_prefix in ["lenskit", "Surprise", "SciKit"]:
+                if n_prefix == "lenskit":
+                    return n_postfix + " (LensKit)"
+                elif n_prefix == "SciKit":
+                    return n_postfix + " (SciKit)"
+                else:
+                    return n_postfix + " (Surprise)"
+            # Filter postfix
+            if n_postfix in ["Regressor", "TabularRegressor"]:
+                return n_prefix
+
+            if n_prefix == "ConstantPredictor":
+                return n_postfix + " (ConstantPredictor)"
+
+        return specific_name
 
     # Top 5 Models per Dataset color coded for LibraryCategory - includes only categories in the top 5
-    heatmap(data_for_rank_plot_models, leg_pos="top")
+    top_5_algo_cats_annot = [
+        ["{:.4f} \n {}".format(x[1], replace_names_for_paper(x[0])) for x in top_5_stats_per_dataset[d_name]] for
+        d_name in data_for_rank_plot_models.index.tolist()]
+    heatmap(data_for_rank_plot_models, leg_pos="top", annot=top_5_algo_cats_annot, fmt="s", annot_kws={"size": "small"})
     plt.xlabel("Rank")
     plt.yticks(rotation=0)
     plt.tight_layout()
     if save_images:
         name = prefix
         name += "ranking_models_per_dataset_with_categories"
-        plt.savefig(get_correct_path(name))
+        # plt.savefig(get_correct_path(name))
     plt.show()
 
     # LibraryCategory Ranking per Dataset - includes all categories
-    heatmap(data_for_rank_plot_libcat, leg_pos="top")
+    top_5_cat_annot = [
+        ["{:.4f} \n {}".format(x[1], replace_names_for_paper(x[0])) for x in top_5_stats_per_cat_per_dataset[d_name]]
+        for d_name in data_for_rank_plot_models.index.tolist()]
+    heatmap(data_for_rank_plot_libcat, leg_pos="top", annot=top_5_cat_annot, fmt="s", annot_kws={"size": "small"})
     plt.xlabel("Rank")
     plt.yticks(rotation=0)
     plt.tight_layout()
     if save_images:
         name = prefix
         name += "ranking_categories_per_dataset"
-        plt.savefig(get_correct_path(name))
+        # plt.savefig(get_correct_path(name))
     plt.show()
