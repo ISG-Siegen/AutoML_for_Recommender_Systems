d66716794151919f014c9f85a3ec7d557d816969
tvnte <tobi-vente@web.de>
2022-02-28 15:56:26 +0100

    Update References for general_utils

--- general_utils/catheat.py
+++ general_utils/catheat.py
@@ -1,21 +1,22 @@
-# Script take from the source below to integrate it directly without having to use other install workaround
+"""
+    Script taken from the source below to integrate it directly without having to use other install workaround
 
-#    This script is part of pymaid (http://www.github.com/schlegelp/catheat).
-#    Copyright (C) 2017 Philipp Schlegel
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along
+    This script is part of pymaid (http://www.github.com/schlegelp/catheat).
+    Copyright (C) 2017 Philipp Schlegel
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along
+"""
 
 import seaborn as sns
 import pandas as pd
